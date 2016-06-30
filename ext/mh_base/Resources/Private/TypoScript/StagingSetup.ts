8f6772530ce22d8a99a1c5ff2173989da5ebc205
Marc Hauschildt <mhauschildt@medienwerft.de>
2016-06-30 17:46:36 +0200

    [TASK] Development

---
+++ ext/mh_base/Resources/Private/TypoScript/StagingSetup.ts
@@ -0,0 +1,4 @@
+### This part should be removed after starting development ###
+page.20 = TEXT
+page.20.value = <p>You are currently on TYPO3 CONTEXT: <strong>Production Staging</strong></p>
+### END ###
